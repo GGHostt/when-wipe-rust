442415888f4edb9948d7298d6d7dd8b19c2923fe
GGHostt <147630652+GGHostt@users.noreply.github.com>
2026-04-11 17:03:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,8 +40,10 @@ def time_until_friday_18():
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}, вот список команд для работы с ботом: !", \n, "/wipe")
-
+    await message.answer(
+        f"Привет, {html.bold(message.from_user.full_name)}! \n"
+        f"Вот список команд для работы с ботом:\n"
+        f"/wipe — отсчет до следующего вайпа в пятницу"
 
 @dp.message(Command("wipe"))
 async def command_wipe(message: Message) -> None:
